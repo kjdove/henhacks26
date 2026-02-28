e0312d70a121e1b0f39a584041efd44dd09918ec
Kacey Dove <kjdove@udel.edu>
2026-02-28 11:50:46 -0500

    navigation

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {SheriffDashboard} from './sheriffDashboard.tsx'
+import {HashRouter, Route, Routes} from "react-router-dom";
+import { PublicDashboard } from './publicDashboard.tsx'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/SheriffDashboard" element={<SheriffDashboard/>}/>
+        <Route path="/PublicDashboard" element={<PublicDashboard/>}/>
+      </Routes>
+    </HashRouter>
   </StrictMode>,
 )
