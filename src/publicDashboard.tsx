c862f4c49f7bb57f6879f133b9f962cd5de3d91a
Kacey Dove <kjdove@udel.edu>
2026-02-28 16:09:01 -0500

    added images to dashboard

--- src/publicDashboard.tsx
+++ src/publicDashboard.tsx
@@ -1,7 +1,9 @@
 
+import React from 'react';
 import {useNavigate} from 'react-router-dom';
+import './publicDashboard.css';
 
-export function PublicDashboard() {
+export function PublicDashboard(): React.JSX.Element {
     const navigate = useNavigate();
 
     return (
@@ -17,9 +19,21 @@ export function PublicDashboard() {
              </div>
              <p className= 'logout' onClick={() => navigate(`/`)}>Logout</p>
         </div>
-        <div className='sd-content'>
-             <h1>Welcome!</h1>
-             <p>Here you can manage your wanted and caught bandits, as well as view the map of the area.</p>
+        <div className='pd-content'>
+             <h1>Welcome Public!</h1>
+             <p>Here you can view wanted and caught bandits in your area, as well as view a map of where they were last seen.</p>
+             <div className='bandits'>
+                <figure className="marquee">
+                    <img className='marquee_item' src="src/assets/bart_bandit.png" width="200" height="200" alt="BartBandit"/>
+                    <img className='marquee_item' src="src/assets/kat_bandit.JPEG" width="200" height="200" alt="KatBandit"/>
+                    <img className='marquee_item' src="src/assets/liao_bandit.JPEG" width="200" height="200" alt="LiaoBandit"/>
+                    <img className='marquee_item' src="src/assets/mattsap_bandit.JPEG" width="200" height="200" alt="MattBandit"/>
+                    <img className='marquee_item' src= "src/assets/nazim_bandit.JPEG" width="200" height="200" alt= "NazimBandit"/>
+                    <img className='marquee_item' src="src/assets/roosen_bandit.JPEG" width="200" height="200" alt="RoosenBandit"/>
+                    <img className='marquee_item' src="src/assets/silber_bandit.JPEG" width="200" height="200" alt="SilberBandit"/>
+                    <img className='marquee_item' src="src/assets/yarrington_bandit.JPEG" width="200" height="200" alt="YarringtonBandit"/>
+                </figure>  
+             </div>
         </div>
      </div>
     )
