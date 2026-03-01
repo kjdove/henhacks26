bc67f3ed41c08e8a74f7d1d4a005a932a3ba0d3a
Kacey Dove <kjdove@udel.edu>
2026-02-28 21:26:23 -0500

    css

--- src/map.tsx
+++ src/map.tsx
@@ -6,48 +6,19 @@ import { useDashboardNavigation } from './navigation';
 
 export function Map () {
     const navigate = useNavigate();
-
-    const navigateDashboard = () => {
-        if (window.location.pathname.includes("SheriffDashboard")) {
-            navigate("/SheriffDashboard");
-        } else {
-            navigate("/PublicDashboard");
-        }
-    }
-    const navigateWanted = () => {
-        if (window.location.pathname.includes("SheriffDashboard")) {
-            navigate("/SheriffDashboard/Wanted");
-        } else {
-            navigate("/PublicDashboard/Wanted");
-        }
-    }
-    const navigateCaught = () => {
-        if (window.location.pathname.includes("SheriffDashboard")) {
-            navigate("/SheriffDashboard/Caught");
-        } else {
-            navigate("/PublicDashboard/Caught");
-        }
-    }
-    const navigateMap = () => {
-        if (window.location.pathname.includes("SheriffDashboard")) {
-            navigate("/SheriffDashboard/Map");
-        } else {
-            navigate("/PublicDashboard/Map");
-        }
-    }
     const [Distances, setDistances] = useState<number[]>([]);
 
 function getDistanceFromLatLonInM(lat1:number,lon1:number,lat2:number,lon2:number) {
-  var R = 6371; // Radius of the earth in km
-  var dLat = deg2rad(lat2-lat1);  // deg2rad below
-  var dLon = deg2rad(lon2-lon1); 
-  var a = 
+  const R = 6371; // Radius of the earth in km
+  const dLat = deg2rad(lat2-lat1);  // deg2rad below
+  const dLon = deg2rad(lon2-lon1); 
+  const a = 
     Math.sin(dLat/2) * Math.sin(dLat/2) +
     Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
     Math.sin(dLon/2) * Math.sin(dLon/2)
     ; 
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-  var d = R * c; // Distance in km
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  const d = R * c; // Distance in km
   const miles = (d * 0.621371)
   setDistances(prev => [...prev, miles]);
 }
@@ -83,7 +54,7 @@ navigator.geolocation.getCurrentPosition((position) => {
 
     return (
         <>
-           <div className = "page-header">
+            <div className = "page-header">
                 <div className='left'>
                     {/* <h1 onClick={() => goTo()}>Bandit Board</h1> */}
                     <img onClick={() => goTo()} src="./bblogo.png" alt="BanditBoradLogo" height="150px" width="250px"/>
@@ -95,28 +66,30 @@ navigator.geolocation.getCurrentPosition((position) => {
                     <h2 onClick={() => goTo("/Map")}>MAP</h2>
                 </div>
                 <p onClick={() => navigate(`/`)}>LOGOUT</p>
-           </div>
+            </div>
             <div className="map-container">
-  <div className="map-text">
-    <p>
-      Here you can view the map of the area and track the locations of bandits and law enforcement.
-    </p>
+                <div className="map-text">
+                    <p>
+                    Here you can view the map of the area and track the locations of bandits and law enforcement.
+                    </p>
+                    <p>
+                    How can I find these evil criminals?
+                    </p>
 
-    <div>
-      How can I find these evil criminals?
-      <Button className="outline-btn" onClick={() => GetLocation()}>Find out now!</Button>
+                    <div>
+                    
+                    <Button className="outline-btn" onClick={() => GetLocation()}>Find out now!</Button>
 
-      <ul>
-        {Distances.map((num: number, index: number) => (
-          <li key={index}>{num.toFixed(2)} miles</li>
-        ))}
-      </ul>
-    </div>
-  </div>
+                    <ul>
+                        {Distances.map((num: number, index: number) => (
+                        <li key={index}>{num.toFixed(2)} miles</li>
+                        ))}
+                    </ul>
+                    </div>
+                </div>
 
-  <img src="Map.jpg" alt="banditmap" />
-</div>
-            
+                <img src="Map.jpg" alt="banditmap" />
+            </div>
         </>
     )
 }
